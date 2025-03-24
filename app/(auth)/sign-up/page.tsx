06724ca6a88e2a8cd48107f53636745aaf5779b2
tonydz <tonydzneladze@gmail.com>
2025-03-24 22:13:28 +0400

    MobileNav and NavLinks

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import AuthForm from '@/components/forms/AuthForm';
+import { SignUpSchema } from '@/lib/validation';
 import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>page</div>
+    <AuthForm 
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => promise.resolve({success: true, data})}
+    />
   )
 }
 
