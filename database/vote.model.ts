50ea9d7d268c6bc3e45ee5f90852935dcf70b2e4
tonydz <tonydzneladze@gmail.com>
2025-04-14 21:59:35 +0400

    implement interaction model V2

--- database/vote.model.ts
+++ database/vote.model.ts
@@ -1,18 +1,22 @@
-import { models, Schema, Types, model } from "mongoose";
+import { model, models, Schema, Types } from "mongoose";
 
 export interface IVote {
-    author: Types.ObjectId;
-    id: Types.ObjectId;
-    type: "question" | "answer";
-    voteType: "upvote" | "downvote";
+  author: Types.ObjectId;
+  actionId: Types.ObjectId;
+  actionType: "question" | "answer";
+  voteType: "upvote" | "downvote";
 }
 
-const VoteSchema = new Schema<IVote>({
-    author: {type: Schema.Types.ObjectId, ref: "User", required: true},
-    id: {type: Schema.Types.ObjectId, required: true},
-    type: {type: String, enum: ["question", "answer"], required: true},
-    voteType: {type: String, enum: ["upvote", "downvote"], required: true},
-}, {timestamps: true});
+export interface IVoteDoc extends IVote, Document {}
+const VoteSchema = new Schema<IVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  { timestamps: true }
+);
 
 const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
 
