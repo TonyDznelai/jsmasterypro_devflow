52000a838b85a18e4ce96905d647b843fbf3508c
tonydz <tonydzneladze@gmail.com>
2025-04-19 17:12:57 +0400

    implement route handlers

---
+++ app/database.ts
@@ -0,0 +1,34 @@
+const tickets = [
+    {
+        id: 1,
+        name: "Fix next.js API route bug",
+        status: "open",
+        type: "bug",
+    },
+    {
+        id: 2,
+        name: "Implement new feature",
+        status: "in-progress",
+        type: "feature",
+    },
+    {
+        id: 3,
+        name: "Update documentation",
+        status: "closed",
+        type: "documentation",
+    },
+    {
+        id: 4,
+        name: "Fix CSS styling issue",
+        status: "open",
+        type: "bug",
+    },
+    {
+        id: 5,
+        name: "Add new API endpoint",
+        status: "in-progress",
+        type: "feature",
+    },
+]
+
+export default tickets;
