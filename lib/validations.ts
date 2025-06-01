53647ebf367665cef4e85b94801d5f9e04eb2952
tonydz <tonydzneladze@gmail.com>
2025-06-01 18:10:01 +0400

    Implement votes server actions

--- lib/validations.ts
+++ lib/validations.ts
@@ -167,3 +167,13 @@ export const AIAnswerSchema = z.object({
   content: z.string().min(100, { message: "Answer has to be more than 100 characters." }),
   userAnswer: z.string().optional(),
 });
+
+export const CreateVoteSchema = z.object({
+  targetId: z.string().min(1, { message: "Target ID is required." }),
+  targetType: z.enum(["question", "answer"], {message: "Target type must be either 'question' or 'answer'."}),
+  voteType: z.enum(["upvote", "downvote"], { message: "Vote type must be either 'upvote' or 'downvote'." }),
+});
+
+export const UpdateVoteCountSchema = CreateVoteSchema.extend({
+  change: z.number().int().min(-1).max(1),
+});
