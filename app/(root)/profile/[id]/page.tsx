464cf2a91d77ece92d59093522658a475ae4a4aa
tonydz <tonydzneladze@gmail.com>
2025-06-16 21:39:11 +0400

    implement tabs layout

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProfileLink from "@/components/user/ProfileLink";
 import Stats from "@/components/user/Stats";
 import UserAvatar from "@/components/UserAvatar";
@@ -44,7 +45,9 @@ const Profile = async ({ params }: RouteParams) => {
 
           <div className="mt-3">
             <h2 className="h2-bold text-dark100_light900">{name}</h2>
-            <p className="paragraph-regular text-dark200_light800">@{username}</p>
+            <p className="paragraph-regular text-dark200_light800">
+              @{username}
+            </p>
 
             <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
               {portfolio && (
@@ -81,16 +84,41 @@ const Profile = async ({ params }: RouteParams) => {
           )}
         </div>
       </section>
-    
-    <Stats 
-      totalQuestions={totalQuestions}
-      totalAnswers={totalAnswers}
-      badges={{
-        GOLD: 0,
-        SILVER: 0,
-        BRONZE: 0,
-      }}
-    />
+
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{
+          GOLD: 0,
+          SILVER: 0,
+          BRONZE: 0,
+        }}
+      />
+
+      <section className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-[2]">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">Top Posts</TabsTrigger>
+            <TabsTrigger value="answers" className="tab">Answers</TabsTrigger>
+          </TabsList>
+          <TabsContent value="top-posts" className="mt-5 flex w-full flex-col gap-6">
+            List of questions
+          </TabsContent>
+          <TabsContent value="answers" className=" flex w-full flex-col gap-6">
+            List of answers
+          </TabsContent>
+        </Tabs>
+
+        <div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
+          <h3 className="h3-bold text-dark200_light900">Top Tech</h3>
+          
+          <div className="mt-7 flex flex-col gap-4">
+            <p>
+              List of Tags
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
