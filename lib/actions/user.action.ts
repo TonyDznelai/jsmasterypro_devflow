6f38b817b48f76d5b4aedee11f98132ac9bd3ae7
tonydz <tonydzneladze@gmail.com>
2025-06-19 20:38:16 +0400

    implement user's questions

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -73,11 +73,13 @@ export async function getUsers(
   }
 }
 
-export async function getUser(params: GetUserParams): Promise<ActionResponse<{
-  user: User;
-  totalQuestions: number;
-  totalAnswers: number;
-}>> {
+export async function getUser(params: GetUserParams): Promise<
+  ActionResponse<{
+    user: User;
+    totalQuestions: number;
+    totalAnswers: number;
+  }>
+> {
   const validationResult = await action({
     params,
     schema: GetUserSchema,
@@ -94,16 +96,59 @@ export async function getUser(params: GetUserParams): Promise<ActionResponse<{
 
     if (!user) throw new Error("User not found");
 
-    const totalQuestions = await Question.countDocuments({author: userId});
-    const totalAnswers = await Answer.countDocuments({answers: userId});
-  
+    const totalQuestions = await Question.countDocuments({ author: userId });
+    const totalAnswers = await Answer.countDocuments({ author: userId });
+
     return {
       success: true,
-      data:{ 
-      user: JSON.parse(JSON.stringify(user)),
-      totalQuestions,
-      totalAnswers,
-      }
+      data: {
+        user: JSON.parse(JSON.stringify(user)),
+        totalQuestions,
+        totalAnswers,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getUserQuestions(params: GetUserQuestionsParams): Promise<
+  ActionResponse<{
+    questions: Question[];
+    isNext: boolean;
+  }>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetUserSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { userId, page = 1, pageSize = 10 } = params;
+
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = pageSize;
+
+  try {
+    const totalQuestions = await Question.countDocuments({ author: userId });
+
+    const questions = await Question.find({ author: userId })
+      .populate("tags", "name")
+      .populate("author", "name image")
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: {
+        questions: JSON.parse(JSON.stringify(questions)),
+        isNext,
+      },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
